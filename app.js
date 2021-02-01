1c2644fcf7e304a9c77eae37e8c1fb1a3d7e5593
Jaden7856 <jhg7856@gmail.com>
2021-02-01 15:59:26 +0900

    added a new end_point

--- app.js
+++ app.js
@@ -5,4 +5,6 @@ const port = process.env.PORT || 3000;
 // http://127.0.0.1:3000/
 app.get('/', (req, res) => res.send('Hello, Nodejs!'));
 app.get('/welcome', (req, res) => res.send('Hi, there. Welcome to the Nodejs'));
+app.get('/check', (req, res) => res.send('Example app listening on port 3000'));
+
 app.listen(port, () => console.log('Example app listening on port 3000'))
